5199d0fccd763d92bc6adcfa516cb4f1226f186a
sksinghanviam <sksingh@anviam.com>
2019-02-26 16:35:00 +0530

    Updates

---
+++ precache-manifest.9a2c36fe0dead18a4f9d69e434ee9c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb8cb15faef4a46f927e",
+    "url": "/sksinghanviam/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "bb8cb15faef4a46f927e",
+    "url": "/sksinghanviam/contactmanager/static/js/main.bb8cb15f.chunk.js"
+  },
+  {
+    "revision": "c60185e8d84c51f5e987",
+    "url": "/sksinghanviam/contactmanager/static/js/runtime~main.c60185e8.js"
+  },
+  {
+    "revision": "54c550745934808a27ec",
+    "url": "/sksinghanviam/contactmanager/static/css/2.2cf1ca15.chunk.css"
+  },
+  {
+    "revision": "54c550745934808a27ec",
+    "url": "/sksinghanviam/contactmanager/static/js/2.54c55074.chunk.js"
+  },
+  {
+    "revision": "710754fbaa4cbe7034945b5c44c10ba2",
+    "url": "/sksinghanviam/contactmanager/index.html"
+  }
+];
